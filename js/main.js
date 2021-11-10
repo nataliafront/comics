b78cb36fc637586a9fca72329a65d50140daf2f1
Natalia Panina <93536861+nataliafront@users.noreply.github.com>
2021-11-10 21:45:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,91 @@
+var swiper = new Swiper(".comics-slider1", {
+  slidesPerView: 1,
+
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
+
+var swiper = new Swiper(".comics-slider2", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
+
+var swiper = new Swiper(".comics-slider3", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  breakpoints: {
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 0,
+    },
+
+    950: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+
+    680: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
